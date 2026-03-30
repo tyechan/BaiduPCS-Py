48be31d85faf4f918950e0aff4cc93bc79c3baac
tyechan <maplecty@hotmail.com>
2026-03-30 16:40:33 +0900

    fix: Adding find_packages(include=["baidupcs_py", "baidupcs_py.*"]) tells setuptools exactly which packages to include

--- setup.py
+++ setup.py
@@ -5,4 +5,7 @@
 import setuptools
 
 if __name__ == "__main__":
-    setuptools.setup(name="baidupcs-py")
+    setuptools.setup(
+        name="baidupcs-py",
+        packages=setuptools.find_packages(include=["baidupcs_py", "baidupcs_py.*"]),
+    )
