7adffbf10c773cb31742d48513fc1ad787b2b628
tyechan <maplecty@hotmail.com>
2026-03-30 16:55:32 +0900

    refactor: restructure setup process to use setup_kwargs and build function

--- setup.py
+++ setup.py
@@ -3,9 +3,12 @@
 # This is a shim to hopefully allow Github to detect the package, build is done with poetry
 
 import setuptools
+from build import build
 
 if __name__ == "__main__":
-    setuptools.setup(
-        name="baidupcs-py",
-        packages=setuptools.find_packages(include=["baidupcs_py", "baidupcs_py.*"]),
-    )
+    setup_kwargs = {
+        "name": "baidupcs-py",
+        "packages": setuptools.find_packages(include=["baidupcs_py", "baidupcs_py.*"]),
+    }
+    build(setup_kwargs)
+    setuptools.setup(**setup_kwargs)
